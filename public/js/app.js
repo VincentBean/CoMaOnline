14a7da09317817a7b7cc7a8a4225084a578e78fd
vayku <vaykutv@gmail.com>
2019-05-10 11:10:14 +0200

    Initializing authentication, applied spatie/permission, migrations

--- public/js/app.js
+++ public/js/app.js
@@ -49437,8 +49437,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Github\CoMaOnline\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Github\CoMaOnline\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\CoMaOnline\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\CoMaOnline\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
