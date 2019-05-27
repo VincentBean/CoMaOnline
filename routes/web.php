c71125a497ff8625dd45f733af8b81199dc38968
Faruk Bikmaz <FarukBikmaz@outlook.com>
2019-05-27 13:45:28 +0200

    web test

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,10 @@ Route::prefix('producten')->group(function () {
     Route::get('{category}', 'ProductsController@category')->name('home.category.details');
     Route::get('product/{id}', 'ProductsController@details')->name('home.product');
 });
+
+Route::get('/register', 'RegistrationController@create');
+Route::post('register', 'RegistrationController@store');
+ 
+Route::get('/login', 'SessionsController@create');
+Route::post('/login', 'SessionsController@store');
+Route::get('/logout', 'SessionsController@destroy');
